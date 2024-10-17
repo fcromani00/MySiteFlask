cc4f6317fdab6fd065f0103d7cfa186d4e50a9e4
fcromani00 <113251317+fcromani00@users.noreply.github.com>
2024-10-17 15:01:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 
-# Configura Flask para usar o diretório 'assets' para arquivos estáticos
 app = Flask(__name__, static_folder='assets')
 
-# Rota para a página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
